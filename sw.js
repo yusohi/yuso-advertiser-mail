b002113697bcf2db0897b1b5010493976b05d6fb
yusohi <dbthwjd97@gmail.com>
2026-06-12 17:46:29 +0900

    Show mobile drawer labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "yuso-mail-v2026-06-12-readable-list";
+const CACHE_VERSION = "yuso-mail-v2026-06-12-mobile-drawer-labels";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
